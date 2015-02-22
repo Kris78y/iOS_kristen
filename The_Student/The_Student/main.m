22ea69f9d73e2738358f3931fef42e582da4e674
Yew Bee Wah <kyew@wiley.com>
2015-02-22 15:09:06 +0800

    The_Student First Commit

---
+++ The_Student/The_Student/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  The_Student
+//
+//  Created by Yew Bee Wah on 22/2/15.
+//  Copyright (c) 2015 Yew Bee Wah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
